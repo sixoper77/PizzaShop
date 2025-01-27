dfcaaefdb971f1538d59c6723fc1d7174e9b3287
Andrey <andrrepka2088@gmail.com>
2025-01-28 01:08:00 +0200

    progress d1

---
+++ app2/main/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+app_name="main"
+urlpatterns = [
+    path('',views.popular_list,name='popular_list'),
+    path('<slug:slug>/',views.producr_detail,name='product_detail'),
+]
