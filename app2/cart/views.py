3b0ce300edbdf709c30780c2e3e4e4c4f212bced
Andrey <146370343+ternandrey124532@users.noreply.github.com>
2025-02-01 16:35:52 +0200

    Cart 50%

--- app2/cart/views.py
+++ app2/cart/views.py
@@ -8,3 +8,19 @@ from .forms import CartAddProductForm
 def card_add(request,product_id):
     cart=Cart(request)
     product=get_object_or_404(Products,id=product_id)
+    form=CartAddProductForm(request.POST)
+    if form.is_valid():
+        cd=form.cleaned_data
+        cart.add(product=product,quantity=cd['quantity'],override_quantity=cd['override'])
+    return redirect('cart:cart_detail')
+
+@require_POST
+def cart_remove(request,product_id):
+    cart=Cart(request)
+    product=get_object_or_404(Products,id=product_id)
+    cart.remove(product)
+    return redirect('cart:cart_detail')
+
+def cart_detail(request):
+    cart=Cart(request)
+    return render(request,'cart/detail.html',{'cart':cart})
