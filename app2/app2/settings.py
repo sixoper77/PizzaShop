70b112e5b83247fb13bf6ed18549c764549882ee
Andrey <146370343+ternandrey124532@users.noreply.github.com>
2025-02-03 23:58:51 +0200

    Cart is done. Registration is in progress.

--- app2/app2/settings.py
+++ app2/app2/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'main',
     'cart',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -78,8 +79,12 @@ WSGI_APPLICATION = 'app2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'pizza',
+        'USER':'pizza',
+        'PASSWORD':'pizza',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
