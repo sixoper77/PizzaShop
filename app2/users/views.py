a2300a1261c36d7ceb275909b78ca6bc476eea58
Andrey <146370343+ternandrey124532@users.noreply.github.com>
2025-02-11 21:09:59 +0200

    Add files via upload

--- app2/users/views.py
+++ app2/users/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
-from django.contrib import auth
+from django.shortcuts import render,redirect
+from django.contrib import auth,messages
 from django.urls import reverse
 from django.http import HttpResponseRedirect
-from .forms import UserLoginForm
-
+from .forms import UserLoginForm,UserRegistrationForm,ProfileForm
+from django.contrib.auth.decorators import login_required
+from orders.models import Order,OrderItem
+from django.db.models import Prefetch
 def login(request):
     if request.method=='POST':
         form=UserLoginForm(data=request.POST)
@@ -19,11 +21,34 @@ def login(request):
     return render(request,'users/login.html')
 
 def registration(request):
-    return render(request,'users/register.html')
-
+    if request.method=='POST':
+        form=UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            user=form.instance
+            auth.login(request,user)
+            messages.success(request,f'{user.username},Successful Registration')
+            return HttpResponseRedirect(reverse('user:login'))
+    else:
+        form=UserRegistrationForm()
+    return render(request,'users/registration.html')
+@login_required
 def profile(request):
-    return render(request,'users/profile.html')
+    if request.method == 'POST':
+        form = ProfileForm(data=request.POST, instance=request.user, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile was changed')
+            return HttpResponseRedirect(reverse('user:profile'))
+    else:
+        form = ProfileForm(instance=request.user)
+    orders = Order.objects.filter(user=request.user).prefetch_related(Prefetch(
+        'items',queryset=OrderItem.objects.select_related('product'),)).order_by('-id')
+
+    return render(request, 'users/profile.html',{'form':form,'orders':orders})
+
 
 def logout(request):
-    ...
+    auth.logout(request)
+    return redirect(reverse('main:product_list'))
 
