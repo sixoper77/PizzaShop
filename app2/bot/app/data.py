5318557ff9ac73a71d7c628c23f8339b697cd10d
Andrey <146370343+sixoper77@users.noreply.github.com>
2025-03-30 15:18:21 +0300

    optimization

--- app2/bot/app/data.py
+++ app2/bot/app/data.py
@@ -1,10 +1,9 @@
 import aiohttp
 import asyncio
 async def get_data(category_slug=None):
+    url='http://127.0.0.1:8000/api/get-products/'
     if category_slug:
-        url=f'http://127.0.0.1:8000/api/get-products/{category_slug}'
-    else:
-        url='http://127.0.0.1:8000/api/get-products/'
+        url+=f'{category_slug}/'
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as resp:
             data=await resp.json()
