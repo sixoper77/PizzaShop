35e3ee0d910fdaeeca7d32e8717e1beff8219b28
Andrey <146370343+sixoper77@users.noreply.github.com>
2025-05-05 12:46:31 +0300

    Add files via upload

--- app2/cart/cart.py
+++ app2/cart/cart.py
@@ -2,13 +2,11 @@ from decimal import Decimal
 from django.conf import settings
 from django.core.cache import cache
 from main.models import Products
-
 class Cart:
     def __init__(self, request=None, telegram_id=None):
         self.request = request
         
         if telegram_id:
-            # Для Telegram пользователей используем кэш
             self.cache_key = f"cart_telegram_{telegram_id}"
             self.telegram_id = telegram_id
             cached_cart = cache.get(self.cache_key)
@@ -16,7 +14,6 @@ class Cart:
                 cached_cart = {}
             self.cart = cached_cart
         elif request is not None:
-            # Обычная логика для веб-пользователей
             self.session = request.session
             self.cart_key = settings.CART_SESSION_ID
             cart = self.session.get(self.cart_key)
@@ -24,7 +21,6 @@ class Cart:
                 self.session[self.cart_key] = {}
             self.cart = self.session[self.cart_key]
         else:
-            # Пустая корзина, если нет ни request, ни telegram_id
             self.cart = {}
     
     def add(self, product, quantity=1, override_quantity=False):
@@ -44,10 +40,8 @@ class Cart:
         
     def save(self):
         if hasattr(self, 'cache_key'):
-            # Для Telegram пользователей сохраняем в кэш
             cache.set(self.cache_key, self.cart, 3600)
         else:
-            # Для веб-пользователей сохраняем в сессию
             self.session.modified = True
     
     def remove(self, product):
@@ -74,11 +68,9 @@ class Cart:
     
     def clear(self):
         if hasattr(self, 'cache_key'):
-            # Для Telegram пользователей очищаем кэш
             self.cart = {}
             cache.set(self.cache_key, {}, 3600)
         else:
-            # Для веб-пользователей
             if self.cart_key in self.session:
                 del self.session[self.cart_key]
                 self.session.modified = True
